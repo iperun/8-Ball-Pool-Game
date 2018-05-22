0397140b0d6f1938a4f206999f0b188f3306e24a
Ivan Perun <peruni101@gmail.com>
2018-05-22 14:05:03 -0400

    add mouse button click functs

--- input/mouse.js
+++ input/mouse.js
@@ -1,3 +1,31 @@
+function handleMouseMove(evt) {
+
+  let x = evt.pageX;
+  let y = evt.pageY;
+
+  Mouse.position = new Vector2(x, y);
+}
+
+function handleMouseDown(evt) {
+  // Get current position of mouse
+  handleMouseMove(evt);
+  //Properties set and check which buttons are pressed
+  if (evt.which === 1) {
+    if (!Mouse.left.down)
+      Mouse.left.pressed = true;
+    Mouse.left.down = true;
+  } else if (evt.which === 2) {
+    if (!Mouse.middle.down)
+      Mouse.middle.down.pressed = true;
+    Mouse.middle.down = true;
+  } else if (evt.which === 3) {
+    if (!Mouse.right.down)
+      Mouse.right.pressed = true;
+    Mouse.right.down = true;
+  }
+
+}
+
 // Create mouse handler
 function MouseHandler() {
 
@@ -11,3 +39,5 @@ function MouseHandler() {
   document.onmousedown = handleMouseDown;
   document.onmouseup = handleMouseUp;
 }
+// Create mouse object
+let Mouse = new MouseHandler();
