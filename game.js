9486a07f83b70e2441dfa4de435d59e6612eee5a
Ivan Perun <peruni101@gmail.com>
2018-05-22 11:32:13 -0400

    add game funct and game world

---
+++ game.js
@@ -0,0 +1,15 @@
+function Game() {
+
+}
+// Initialize the Game
+Game.prototype.init = function() {
+
+}
+// Start the Game
+Game.prototype.start = function() {
+
+}
+// Start assets
+Game.prototype.mainLoop = function() {
+
+}
---
+++ gameWorld.js
@@ -0,0 +1,13 @@
+// Contains physical objects of game
+function GameWorld() {
+
+}
+// Updates objects in the Game
+GameWorld.prototype.update = function() {
+
+}
+
+// Draws objects in Game
+GameWorld.prototype.draw = function() {
+
+}
