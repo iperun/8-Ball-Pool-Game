d80eedbc843fa00c3f58a099aaf9cc6132e2d969
Ivan Perun <peruni101@gmail.com>
2018-05-22 11:39:42 -0400

    update game and gameworld

--- game.js
+++ game.js
@@ -4,12 +4,26 @@ function Game() {
 // Initialize the Game
 Game.prototype.init = function() {
 
+  this.gameWorld = new GameWorld();
 }
 // Start the Game
 Game.prototype.start = function() {
+
+  this.init();
+  this.mainLoop();
 
 }
 // Start assets
 Game.prototype.mainLoop = function() {
 
+  Canvas.clear();
+  // Update game world
+  PoolGame.gameWorld.update();
+  // Draw game game world
+  PoolGame.gameWorld.draw();
+
+  requestsAnimationFrame(PoolGame.mainLoop);
+
 }
+
+let PoolGame = new Game();
--- gameWorld.js
+++ gameWorld.js
@@ -10,4 +10,6 @@ GameWorld.prototype.update = function() {
 // Draws objects in Game
 GameWorld.prototype.draw = function() {
 
+  Canvas.drawImage(sprites.background, {x:0, y;0});
+  
 }
