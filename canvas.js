adb4b4220f55076e2848e5cfc6479d04f75f2fd7
Ivan Perun <peruni101@gmail.com>
2018-05-22 16:46:06 -0400

    rotation atan2 added

--- canvas.js
+++ canvas.js
@@ -8,7 +8,7 @@ Canvas2D.prototype.clear = function() {
   this._canvasContext.clearRect(0, 0, this._canvas.width, this._canvas.height);
 }
 // Use Canvas Context to draw image at given postitions
-Canvas2D.prototype.drawImage = function(image, position, origin) {
+Canvas2D.prototype.drawImage = function(image, position, origin, rotation = 0) {
 
   if (!position) {
     position = new Vector2();
@@ -20,6 +20,7 @@ Canvas2D.prototype.drawImage = function(image, position, origin) {
 
   this._canvasContext.save();
   this._canvasContext.translate(position.x, position.y);
+  this._canvasContext.rotate(rotation);
   this._canvasContext.drawImage(image, -origin.x, -origin.y);
   this._canvasContext.restore();
 }
--- stick.js
+++ stick.js
@@ -2,6 +2,7 @@ const STICK_ORIGIN = new Vector2(970, 11);
 
 function Stick(position) {
   this.position = position;
+  this.roation = 0;
 }
 
 Stick.prototype.update = function() {
@@ -10,10 +11,18 @@ Stick.prototype.update = function() {
 
   if (Mouse.left.pressed) {
     console.log("pressed left");
-  }
-  */
+  }*/
+  this.updateRotation();
 }
 
 Stick.prototype.draw = function() {
-    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN);
+    Canvas.drawImage(sprites.stick, this.position, STICK_ORIGIN, this.rotation);
     }
+
+Stick.prototype.updateRotation = function(){
+
+  let opposite = Mouse.position.y - this.position.y;
+  let adjacent = Mouse.position.x - this.position.x;
+
+  this.rotation = Math.atan2(opposite, adjacent);
+}
